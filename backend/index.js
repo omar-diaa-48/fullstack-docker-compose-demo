561313995b2e45eb4919d6cf00673bab302e21a8
Omar <omar.elwakil@vodafone.com>
2022-11-12 14:57:18 +0200

    adjust proxies between be and fe

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,17 @@ const app = express();
 
 const PORT = process.env.PORT
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json())
 
 app.get('/products', (req, res) => {
     const products = [{ id: 1, title: 'T-shirt', price: 40 }, { id: 2, title: 'Jacket', price: 110 }]
     res.json(products)
+})
+
+app.use('*', (req, res, next) => {
+    console.log('Hi');
+    next();
 })
 
 app.listen(PORT, () => console.log(`Backend is running on port ${PORT}`))
